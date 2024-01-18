d126095d958cbaf7605729071529f4ef08d91068
Eduardo Arruda <eduardoarruda2001@outlook.com>
2024-01-18 19:32:39 -0300

    Montanha Russa

---
+++ index.py
@@ -0,0 +1,9 @@
+print("Bem-vindo á montanha-russa\nA altura minima para conseguir andar é 150 centimentros!\n")
+altura = int(input("Qual a sua altura?\n"))
+
+if altura >= 150: 
+   print("Você consegue andar na montanha-russa\n")
+else:
+   print ("Você não pode andar na montanha-russa\n")
+
+enter = input("Pressione Enter para fechar")
